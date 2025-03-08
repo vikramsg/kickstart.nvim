23c382fe321c39d833a9b95bf569277d15d41952
Vikram Singh <vikram@MAC>
2025-03-08 21:47:37 +0100

    Setup debugging

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -96,6 +96,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'debugpy',
       },
     }
 
@@ -145,5 +146,7 @@ return {
         detached = vim.fn.has 'win32' == 0,
       },
     }
+
+    require('dap-python').setup {}
   end,
 }
